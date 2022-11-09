6596732bbc5ed82cb7ff2a14358580d1689fe5ef
Jacob-109 <65383310+Jacob-109@users.noreply.github.com>
2022-11-09 10:36:29 +0800

    A2

--- a2_setup.sh
+++ a2_setup.sh
@@ -1,8 +1,25 @@
+# Create Cluster
 kind create cluster --name kind-1 --config k8s/kind/cluster-config.yaml
-docker build -t jacob/otot-a1-nodeserver:latest ./app
-kind load docker-image --name jacob/otot-a1-nodeserver:latest
-kubectl apply -f "k8s/manifests/k8s/backend_deployment.yml"
+# Check nodes created
+kubectl get nodes
+
+# Label node as ingressready
+kubectl label node kind-1-worker ingress-ready=true
+kubectl label node kind-1-worker2 ingress-ready=true
+kubectl label node kind-1-worker3 ingress-ready=true
+
+# Apply backend-deployment
+kubectl apply -f "k8s/manifests/k8s/backend-deployment.yaml"
+
+# Apply backend-service
+kubectl apply -f "k8s/manifests/k8s/backend-service.yaml"
+
+# Build app and load image
+kind load docker-image otot-a1-nodeserver --name kind-1
+
+# Creating ingress controller
 kubectl apply -f "https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml"
-until kubectl -n ingress-nginx get deploy | grep -m 1 "1/1"; do sleep 3 ; done
-kubectl apply -f "k8s/manifests/k8s/backend-service.yml"
-kubectl apply -f "k8s/manifests/k8s/backend-ingress.yml"
+until kubectl -n ingress-nginx get deploy ; do sleep 3m ; done
+
+# Create ingress
+kubectl apply -f "k8s/manifests/k8s/backend-ingress.yaml"
